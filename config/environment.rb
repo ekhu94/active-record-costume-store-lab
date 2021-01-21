a5eddb89d4c69a08eded47a16bb7950b1866c977
Erik Huang <erikhuangdrums@gmail.com>
2021-01-21 15:10:02 -0800

    done

--- config/environment.rb
+++ config/environment.rb
@@ -4,6 +4,9 @@ require "sinatra/activerecord"
 require "ostruct"
 require "date"
 require_all 'app/models'
+# require_relative 'app/models/costume.rb'
+# require_relative 'app/models/costume_store.rb'
+# require_relative 'app/models/haunted_house.rb'
 
 ENV["SINATRA_ENV"] ||= 'development'
 ActiveRecord::Base.establish_connection(ENV["SINATRA_ENV"].to_sym)
